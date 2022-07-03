45f71b4866b9bc1749011a583e0fdc0f35111b47
YungMilky <filtersome@gmail.com>
2022-07-03 13:46:54 +0200

    server s now submodule

--- server/routes/token.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { refreshToken } = require('../controllers/tokenController');
-
-router.route('/refreshToken').post(refreshToken);
-
-module.exports = router;
